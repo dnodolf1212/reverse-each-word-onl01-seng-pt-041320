a4f189d0fe79c9c85df18ead6eed38deac447d45
Dwight Nodolf <dnodolf@gmail.com>
2020-04-21 04:59:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(string)
   new_array = []                #the array we want to return
 
   array.each do |x|             #each method to change each element in array
-  new_array << array.reverse    #puts reversed array in new return array
+  new_array << string.reverse    #puts reversed array in new return array
   end
   new_array.join(" ")        #we join the array elements to puts out the string
             
